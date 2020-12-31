9d6e8419ca902f73dabdab4e831fa3187a5dc2a1
iilibxc <2026262766@qq.com>
2020-12-31 16:45:40 +0800

    sql

--- root.sql
+++ root.sql
@@ -10,10 +10,29 @@ Target Server Type    : MYSQL
 Target Server Version : 50717
 File Encoding         : 65001
 
-Date: 2020-12-29 16:18:47
+Date: 2020-12-31 16:44:40
 */
 
 SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL COMMENT '姓名',
+  `parent_phone` varchar(255) DEFAULT NULL COMMENT '家长电话号码逗号分隔',
+  `city` varchar(255) DEFAULT NULL COMMENT '居住城市json格式存储{"province":"广东省","city":"深圳市","district":"南山区"}',
+  `net_name` varchar(255) DEFAULT NULL COMMENT '网名逗号分隔',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+INSERT INTO `student` VALUES ('1', '小英', '13222222222,13333333333,15777777777', '{\"province\":\"广东省\",\"city\":\"深圳市\",\"district\":\"南山区\"}', '会飞的鱼,小小雪,茉莉香果果');
+INSERT INTO `student` VALUES ('2', '小明', '13222222222,13333333333,15777777777', '{\"province\":\"广东省\",\"city\":\"深圳市\",\"district\":\"南山区\"}', '会飞的鱼2,小小雪2,茉莉香果果2');
 
 -- ----------------------------
 -- Table structure for tb_description
@@ -65,4 +84,4 @@ CREATE TABLE `tb_user` (
 -- ----------------------------
 INSERT INTO `tb_user` VALUES ('46', 'iilibxc', '123456', null, null);
 INSERT INTO `tb_user` VALUES ('47', 'iilibxc', '123456', null, null);
-INSERT INTO `tb_user` VALUES ('48', null, null, null, null);
+INSERT INTO `tb_user` VALUES ('48', 'iilibxc', 'iilibxc', '2020-12-29 16:05:10', '1');
---
+++ root1.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 127.0.0.1
+Source Server Version : 50717
+Source Host           : localhost:3306
+Source Database       : root1
+
+Target Server Type    : MYSQL
+Target Server Version : 50717
+File Encoding         : 65001
+
+Date: 2020-12-31 16:44:55
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
