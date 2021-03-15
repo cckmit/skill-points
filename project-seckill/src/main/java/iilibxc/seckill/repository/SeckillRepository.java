70041c28cf7087277096d6d28a5891426e5ca27f
iilibxc <2026262766@qq.com>
2021-03-15 17:02:04 +0800

    修改文件名

--- project-seckill/src/main/java/iilibxc/seckill/repository/SeckillRepository.java
+++
@@ -1,8 +0,0 @@
-package iilibxc.seckill.repository;
-import iilibxc.seckill.common.entity.Seckill;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SeckillRepository extends JpaRepository<Seckill, Long> {
-	
-	
-}
